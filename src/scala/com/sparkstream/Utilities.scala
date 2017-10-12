91bbba88710db29b8617e9a83fa012d75da4aa44
shrutitanwar <shruti.tanwar93@gmail.com>
2017-10-12 16:37:46 +0530

    added Main and twitter resources

---
+++ src/scala/com/sparkstream/Utilities.scala
@@ -0,0 +1,21 @@
+package com.sparkstream
+
+import org.apache.log4j.Level
+import java.util.regex.Pattern
+import java.util.regex.Matcher
+
+object Utilities {
+  
+  /** Configures Twitter service credentials using twiter.txt in the main workspace directory */
+  def setupTwitter() = {
+    import scala.io.Source
+    
+    for (line <- Source.fromFile("../twitter.txt`").getLines) {
+      val fields = line.split(" ")
+      if (fields.length == 2) {
+        System.setProperty("twitter4j.oauth." + fields(0), fields(1))
+      }
+    }
+  }
+}
+
